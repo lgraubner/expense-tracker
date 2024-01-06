81066740b911c652a6422a1582a3e36924196ab6
Lars Graubner <mail@larsgraubner.de>
2024-01-06 15:13:35 +0100

    feat: adjust current day date display

--- src/lib/components/date-heading.svelte
+++ src/lib/components/date-heading.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-	import { format } from 'date-fns';
+	import { format, isToday, isYesterday } from 'date-fns';
 
 	export let date: Date;
+
+	function formatDate(date: Date): string {
+		if (isToday(date)) {
+			return 'Today';
+		}
+
+		if (isYesterday(date)) {
+			return 'Yesterday';
+		}
+
+		return format(date, 'EEEE d LLLL');
+	}
 </script>
 
-<div class="text-sm font-medium uppercase">{format(date, 'EEEE d LLLL')}</div>
+<div class="text-sm font-medium uppercase">{formatDate(date)}</div>
