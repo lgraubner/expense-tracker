d4ec5ccbfb7776ef915403a47593b387d873b982
Lars Graubner <mail@larsgraubner.de>
2024-01-14 13:03:09 +0100

    fix: import path

--- src/routes/(app)/(nav)/+page.server.ts
+++ src/routes/(app)/(nav)/+page.server.ts
@@ -2,7 +2,7 @@ import { BookingType } from '@prisma/client';
 import { startOfMonth } from 'date-fns';
 import { isAuthenticated } from '$lib/server/auth';
 import prisma from '$lib/server/prisma';
-import type { BookingWithCategory } from '../api/bookings/+server';
+import type { BookingWithCategory } from '../../api/bookings/+server';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
